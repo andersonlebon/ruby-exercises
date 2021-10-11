9a6c89435b7a57e8f3ecda35215f1b1223695b97
Caleb Anderson <buyananderson@gmail.com>
2021-10-11 21:59:03 +0200

    lear the length, downcase, upcase, strip methods of strings

--- basics.rb
+++ basics.rb
@@ -1,6 +1,7 @@
-name = "calebee";
+name = "      Caleb is     20   ";
 
 puts " @Caleb in verry strong";
 puts "Caleb is 20";
 puts "Caleb like helping people";
 puts "He is in love with programming"
+puts name.include? "200"

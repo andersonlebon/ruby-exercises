ecaa63256538804920d004fa39703f9214054bf5
Caleb Anderson <buyananderson@gmail.com>
2021-10-11 23:19:13 +0200

    Play with some math methodes

--- basics.rb
+++ basics.rb
@@ -1,7 +1,9 @@
 name = "      Caleb is     20   ";
+number = -20.22722;
 
 puts " @Caleb in verry strong";
 puts "Caleb is 20";
 puts "Caleb like helping people";
 puts "He is in love with programming"
-puts name.index('C')
+puts name.strip()
+puts (((number.abs() / 11.0).round()) *20 /7.0).floor()

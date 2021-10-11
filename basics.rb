394949cbbc8f1f48da7a7c0d256559911d4fc3e0
Caleb Anderson <buyananderson@gmail.com>
2021-10-11 23:50:13 +0200

    Build an addition calculator and use the method to_i and to_s

--- basics.rb
+++ basics.rb
@@ -1,12 +1,6 @@
-name = "      Caleb is     20   ";
-number = -20.22722;
+puts "Enter number 1"
+num1 = gets.chomp().to_i
+puts "Enter number 2"
+num2 = gets.chomp().to_i
 
-puts " @Caleb in verry strong";
-puts "Caleb is 20";
-puts "Caleb like helping people";
-puts "He is in love with programming"
-puts name.strip()
-puts (((number.abs() / 11.0).round()) *20 /7.0).floor()
-puts "Hello man may you put your name please?"
-# user = gets
-puts ("yes of course my name is " + gets)
+puts ("the result is: " + (num1+num2).to_s)

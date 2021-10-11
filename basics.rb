551f4a822a8799a988d152f489b0465e15b39b2f
Caleb Anderson <buyananderson@gmail.com>
2021-10-12 00:38:40 +0200

    Use methods as function in ruby and if conditions

--- basics.rb
+++ basics.rb
@@ -1,6 +1,16 @@
 puts "Enter number 1"
 num1 = gets.chomp().to_f
+puts "Enter the oparator"
+opar = gets.chomp()
 puts "Enter number 2"
 num2 = gets.chomp().to_i
 
-puts ("the result is: " + (num1+num2).to_s)
+def calculat(num1, opar, num2)
+    if opar === '+'
+        return num1+num2
+    elsif opar === '-' 
+        num1-num2
+    end
+end
+
+puts calculat(num1, opar, num2)
